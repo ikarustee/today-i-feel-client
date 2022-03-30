9efba164f20b4ad22981f49b9c08b52c14ee7b1a
Tanja Karius <tanja.karius@googlemail.com>
2022-03-30 19:26:05 +0200

    Add show more button. Add files to gitignore

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './Components/Theme'
-import Fonts from './Components/Fonts'
+// import Fonts from './Components/Fonts'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -12,7 +12,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
-        <Fonts />
+        {/* <Fonts /> */}
           <App />
       </ChakraProvider>
     </BrowserRouter>
