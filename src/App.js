3851072d71b53f42869d2a337dc1a7426849a117
Tanja Karius <tanja.karius@googlemail.com>
2022-03-31 11:38:29 +0200

    Add basic routes. Footer, nav and stuff

--- src/App.js
+++ src/App.js
@@ -3,33 +3,32 @@ import "./fonts.css"
 import { Routes, Route, Link } from "react-router-dom";
 import {Container, useColorMode} from "@chakra-ui/react"
 import Home from "./Components/Home"
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import Footer from "./Components/Footer"
 import ThemeSwitcher from "./Components/ThemeSwitcher"
 import logoLight from "./img/logo-light@2x.png"
 import logoDark from "./img/logo-dark@2x.png"
 
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode()
+
 
   return (
     <div className="App">
       <Container maxW='1100px' centerContent>
-      <header>
-        <Link to="/" className="logo">
-          {colorMode === 'light' ? 
-          (<img src={logoLight} alt="" width={400}/>) 
-          : (<img src={logoDark} alt="" />)
-          }
-        </Link>
-      <ThemeSwitcher />
-      </header>
+      <Navbar />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="about" element={<About />} /> */}
+          {/* <Route path="/articles" element={<Articles />} /> */}
+          {/* <Route path="/articles/:id" element={<SingleArticle />} /> */}
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/about" element={<About />} /> */}
         </Routes>
       </main>
       </Container>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <><h2>Login area</h2></>
+  )
+}
+
+export default Login
