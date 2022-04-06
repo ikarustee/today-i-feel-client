63efead1549e232b46caa79e8a49796d7fdbe172
Tanja Karius <tanja.karius@googlemail.com>
2022-04-06 12:15:49 +0200

    Article list

--- src/Contexts/ArticleContext.js
+++ src/Contexts/ArticleContext.js
@@ -1,18 +1,22 @@
 import React, { createContext, useState, useEffect } from "react";
-import articleDATA from "../helper/articles.json"
+import { useLocation } from "react-router-dom";
+// import articleDATA from "../helper/articles.json"
+import axios from "axios";
 
 export const ArticleContext = createContext()
 
-let blogPosts = articleDATA
+// let blogPosts = articleDATA
 const ArticleContextProvider = ({children}) => {
     const [articles, setArticles] = useState([])
     const [isLoading, setIsloading] = useState(true)
+    let location = useLocation()
 
     async function getArticles() {
+        const URL = "https://todayifeel-server.herokuapp.com/articles"
         try {
-            // const response = await fetch(URL + location.search)
-            // let blogPosts = await response.json()
-            // console.log(data)
+            const response = await axios.get(URL +  location.search)
+            let blogPosts = await response.data
+            // console.log(blogPosts)
             blogPosts = blogPosts.map((a) => {
                 return {
                     ...a,
@@ -25,10 +29,6 @@ const ArticleContextProvider = ({children}) => {
             console.log(error)
         }
     }
-
-    // useEffect(() => {
-    //     getArticles()
-    // },[])
 
     useEffect(() => {
         if(articles.length) return setIsloading(false)
