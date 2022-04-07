7f93de32bb171dcce7f965dca2984fc481534312
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-04-07 10:53:11 +0200

    Adding indicidual Errormessages

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -14,19 +14,29 @@ import {
   } from '@chakra-ui/react';
   import axios from 'axios';
   import { useNavigate } from 'react-router-dom';
+import { Label } from 'recharts';
   
   export default function SimpleCard() {
     const navigate = useNavigate();
-    function signupUser(){
+    async function signupUser(){
+      document.getElementById("emailError").value = "";
+      document.getElementById("passwordError").value = "";
       const email = document.getElementById("email").value;
       console.log(email)
       const password = document.getElementById("password").value;
       console.log(password)
       let url = "https://todayifeel-server.herokuapp.com/user"
-      axios.post(url,{email:email,password:password},{withCredentials:true}).then((response)=> {
-          navigate("/adminDashboard");
-        console.log(response)
-        })
+      let response = await axios.post(url,{email:email,password:password},{withCredentials:true});
+      let data = response.data;
+      if(data.errors){
+        console.log(data.errors)
+        document.getElementById("emailError").innerHTML = data.errors.email;
+        document.getElementById("passwordError").innerHTML = data.errors.password;
+      } else {
+        navigate("/adminDashboard");
+      }
+      
+      console.log(response)
     }
     return (
       <Flex
@@ -47,10 +57,12 @@ import {
               <FormControl id="email">
                 <FormLabel>Email address</FormLabel>
                 <Input type="email" name="email" />
+                <FormLabel id="emailError" className='errorMessage'></FormLabel>
               </FormControl>
               <FormControl id="password">
                 <FormLabel>Password</FormLabel>
                 <Input type="password" name="password" />
+                <FormLabel id="passwordError" className='errorMessage'></FormLabel>
               </FormControl>
               <Stack spacing={10}>
                 <Stack
