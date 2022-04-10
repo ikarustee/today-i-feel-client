a8ad5a905deb63183c2f5629017515c8c39d1ec7
Tanja Karius <tanja.karius@googlemail.com>
2022-04-10 15:12:24 +0200

    Report form on aricles

--- src/Components/SingleArticle.js
+++ src/Components/SingleArticle.js
@@ -2,6 +2,13 @@ import React, {useContext, useState, useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import {
     Box,
+    FormControl,
+    Input,
+    Button,
+    Radio, 
+    RadioGroup,
+    Textarea,
+    Stack,
     Heading,
     Link,
     Tag,
@@ -14,13 +21,15 @@ import DotLoader from "react-spinners/DotLoader";
 import { css } from "@emotion/react";
 import ReactMarkdown from "react-markdown";  
 import {readableDate} from "../helper/dateformatter"
+import axios from 'axios';
 
 const SingleArticle = () => {
     const {articles, isLoading, getArticles} = useContext(ArticleContext)
     const {id} = useParams()
     const thisArticle = articles.find((a) => a.id === id)
-    console.log(thisArticle)
-
+    // console.log(thisArticle)
+    const [value, setValue] = useState("")
+    const [userInput, setUserInput] = useState("")
     const bg = useColorModeValue('blue.300', 'blue.900')
     const color = useColorModeValue('white', 'gray.300')
 
@@ -29,6 +38,18 @@ const SingleArticle = () => {
     margin: 0 auto;
     border-color: red;
   `;
+
+    const handleInputChange = (e) => {
+      // console.log(e.target.value)
+      setUserInput(e.target.value)
+      // console.log(userInput)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      // console.log(e)
+      console.log(userInput, value)
+    }
 
     useEffect(() => {
         getArticles()
@@ -41,19 +62,20 @@ const SingleArticle = () => {
     return (
     <>
         <article>
-            <Container p="0">
+            <Container p="0" maxW={"800px"} >
               <Box
                 bg={bg} 
                 color={color}
                 borderRadius="12px"
                 p={8}
                 marginBottom="2rem"
+
                 >
                 <Heading color={color} as="h1" fontSize="2.5rem" textAlign="center">{thisArticle.title}</Heading>
                 <span className="article__date">{readableDate(thisArticle.createdDate)}</span>
               </Box>
                  <ReactMarkdown className="article__content">{thisArticle.body}</ReactMarkdown>
-                <Divider m="2rem 0 0.75rem" />
+                <Divider m="2rem 0 0.85rem" />
                 <em><span className="article__tag label">Tags:</span> &nbsp;
                   {thisArticle.tags.map((t) => {
                     return (
@@ -62,8 +84,33 @@ const SingleArticle = () => {
                       </Tag>
                     )
                   })}
-                <Divider m="0.5rem 0 0" />
                 </em>
+                <Divider m="0.5rem 0 0" />
+                <Box m="2rem 0">
+                <p>Something is wrong in this article? Let us know.</p>
+                <FormControl >
+                  <form onSubmit={handleSubmit} id="report">
+                      <RadioGroup onChange={setValue} value={value}>
+                        <Stack direction='row'>
+                          <Radio value='Wrong information'>Wrong information</Radio>
+                          <Radio value='Outdated'>Outdated</Radio>
+                          <Radio value='Other'>Other</Radio>
+                        </Stack>
+                      </RadioGroup>
+                      <Textarea
+                        value={userInput}
+                        name="issue"
+                        onChange={handleInputChange}
+                        placeholder="Describe the issue"
+                        size='md'
+                      />
+                      <Button 
+                        type="submit"
+                      >
+                      Submit</Button>
+                  </form>
+                </FormControl>
+              </Box>
             </Container>
         </article>
     </>
